ed54c806b22a5567cca98d90a39fbb1a22c5674e
Ivan Derlich <e@ivanderlich.com>
2020-12-05 21:14:09 -0300

    Home finished

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import './Home.css';
+import linktreeLink from '../media/images/linktree.jpeg'
 
 function Home() {
   return (
@@ -6,9 +7,10 @@ function Home() {
       <div className="home-content">
         <div class="title" >Hi I'm Ivan</div>
         <div class="title" >Welcome to my website!</div>
-        <div>I build software infraestructure</div>
-        <div>Let's connect!</div>
-        <div>Social Media Links</div>
+        <div class="elevator-pitch">I'm a full-stack software developer. I can provide you with the software infrastructure to make your ideas become a reality.</div>
+        <div class="elevator-pitch">As a full-stack software developer I focus both on the aesthetics of your system, as well and its correct functioning.</div>
+        <div class="call-to-action">Let's connect!</div>
+        <a href="https://linktr.ee/ivanderlich"><img class="linktree" src={linktreeLink} alt="linktree link" /></a>
       </div>
     </div>
   );
