4b34a0bdae8c86eb1ead0a717a149aab7d68336c
Ivan Derlich <e@ivanderlich.com>
2020-12-09 19:24:36 -0300

    Working on the About section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,30 @@
 import './About.css';
+import linktreeLink from '../media/images/linktree.jpeg'
 
 function About() {
   return (
     <div className="About" id="about"> 
-      About
+      <div class="myself">
+        <div>
+          <div class="title">About</div>
+          <div class="title margin">Myself</div>
+          <div class="elevator-pitch">I'm a full-stack software developer. I can provide you with the software infrastructure to make your ideas become a reality.</div>
+          <div class="elevator-pitch">As a full-stack software developer I focus both on the aesthetics of your system, as well and its correct functioning.</div>
+          <div class="call-to-action">Let's connect!</div>
+        <a href="https://linktr.ee/ivanderlich"><img class="linktree" src={linktreeLink} alt="linktree link" /></a>
+        </div>
+      </div>
+      <div class="skills">
+        <div class="capabilities">
+          <div>Languages</div>
+        </div>
+        <div class="capabilities">
+          <div>Frameworks</div>
+        </div>
+        <div class="capabilities">
+          <div>Skills</div>
+        </div>
+      </div>
     </div>
   );
 }
