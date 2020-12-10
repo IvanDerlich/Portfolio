399e3929ab71ec7751b5af2af3eeedb7eeb44d34
Ivan Derlich <e@ivanderlich.com>
2020-12-10 04:10:11 -0300

    Add Link to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="Footer" id="footer"> 
-      Made by Ivan Derlich 2020 with React.js
+      Made by <a href="http://linktr.ee/ivanderlich">Ivan Derlich</a> 2020 with React.js
     </div>
   );
 }
