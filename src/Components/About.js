c1ea9d92f3fa5a6d625f1c134b9b07dac52afb3a
Ivan Derlich <e@ivanderlich.com>
2020-12-18 12:42:03 -0300

    Reduce spacing in sections and reduce forms

--- src/Components/About.js
+++ src/Components/About.js
@@ -75,7 +75,7 @@ class About extends React.Component {
         <div class="myself">
           <div>
             <div class="title">About</div>
-            <div class="title margin">Myself</div>
+            <div class="title margin">Me</div>
             <div class="elevator-pitch">I'm a full-stack software developer. I can provide you with the software infrastructure to make your ideas become a reality.</div>
             <div class="elevator-pitch">As a full-stack software developer, I focus both on the aesthetics of your system, as well as its correct functioning.</div>
             <div class="call-to-action">Let's connect! </div>
